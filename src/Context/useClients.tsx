186dd7948b3025e313b730fac4afd1743fc6d682
carlos0550 <carlospelinski03@gmail.com>
2025-02-17 21:37:11 -0300

    feat(clients): Hook general que administra logica de clientes

---
+++ src/Context/useClients.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useMemo, useState } from "react"
+import { Clients } from "./Typescript/ClientsTypes"
+import { logic_apis } from "../apis"
+import { message, notification } from "antd"
+function useClients() {
+    const [clients, setClients] = useState<Clients[]>([])
+
+    const createClient = useCallback(async (clientData) => {
+        const url = new URL(logic_apis.clients + "/create-client")
+        const formData = new FormData()
+        try {
+            for (const key in clientData) {
+                formData.append(key, clientData[key])
+            }
+            const response = await fetch(url, {
+                method: "POST",
+                body: formData
+            })
+            const responseData = await response.json()
+            if(!response.ok){
+                
+                throw new Error(responseData.msg || "Error desconocido")
+            }
+            
+            message.success("Cliente creado con exito")
+            setClients([...clients, ...responseData.clients])
+            return true
+        } catch (error) {
+            console.log(error)
+            notification.error({
+                message: "Error al crear el cliente",
+                description: error.message,
+                duration: 3,
+                pauseOnHover: false,
+                showProgress: true
+            })
+            return false
+        }
+    }, [])
+    return useMemo(() => ({
+        clients,
+        createClient
+    }),[
+        clients,
+        createClient
+    ])
+}
+
+export default useClients
