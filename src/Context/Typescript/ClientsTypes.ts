5cacad4c29582db41ce9487c1573b3b05722b6e6
carlos0550 <carlospelinski03@gmail.com>
2025-02-17 21:37:32 -0300

    feat(clients): define interfaces y tipos para los clientes

---
+++ src/Context/Typescript/ClientsTypes.ts
@@ -0,0 +1,10 @@
+export interface Clients {
+    client_id : string,
+    client_dni: number,
+    client_fullname: string,
+    client_email: string,
+    client_adress1: string,
+    client_address2: string,
+    client_city: string,
+    client_phone: number
+}
