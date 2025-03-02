9b17e40741544eb0140bac1c280c6452827a3cd3
carlos0550 <carlospelinski03@gmail.com>
2025-03-02 10:56:49 -0300

    Feat(delivers): Modal para editar/agregar entregas

---
+++ app/src/Debts/Components/ClientDeliversFormModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ClientsInterface } from '../../Context/Typescript/ClientsTypes'
+import { Modal } from '@mantine/core'
+import { RxCross1 } from 'react-icons/rx'
+import DeliversForm from './FinancialDataTableManager/DeliversForm/DeliversForm'
+import { useAppContext } from '../../Context/AppContext'
+
+interface ModalProps {
+    clientData: ClientsInterface
+    closeModal: () => void
+    isEditing?: boolean
+}
+
+function ClientDeliversFormModal({clientData, closeModal, isEditing}: ModalProps) {
+  const {
+    
+  } = useAppContext()
+  return (
+    <Modal
+        opened={true}
+        onClose={() => closeModal()}
+        title={
+            isEditing
+                ? `Editando entrega de ${clientData.client_fullname}`
+                : `Agregando una entrega en ${clientData.client_fullname}`
+        }
+        padding="sm"
+        overlayProps={{
+            backgroundOpacity: 0.6,
+            blur: 5,
+        }}
+        closeButtonProps={{
+            icon: <RxCross1 />,
+        }}
+        size={"xl"}
+    >
+      <DeliversForm clientData={clientData} closeModal={closeModal} isEditing={isEditing}/>
+    </Modal>
+  )
+}
+
+export default ClientDeliversFormModal
