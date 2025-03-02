72872f7f0a0a1a5a6f1ba98d82616728d144819d
carlos0550 <carlospelinski03@gmail.com>
2025-03-02 10:55:56 -0300

    Feat(delivers): Tipado para las entregas

---
+++ app/src/Context/Typescript/DeliversTypes.ts
@@ -0,0 +1,7 @@
+export interface DeliverDataInterface{
+    deliver_id?: string,
+    deliver_user_id?: string,
+    deliver_client_id: string,
+    deliver_amount: string,
+    deliver_date: Date
+}
