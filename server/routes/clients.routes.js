a3ff910e46ffb84638d6ac9073f518839f9477ab
carlos0550 <carlospelinski03@gmail.com>
2025-03-02 10:59:48 -0300

    Feat(delivers): Agrega las rutas para las entregas de dinero

--- server/routes/clients.routes.js
+++ server/routes/clients.routes.js
@@ -1,12 +1,20 @@
 const clientsRouter = require('express').Router();
+
+//Controladores
 const clientsController = require("../controllers/Clients/clients.controller.js")
 const debtsController = require("../controllers/Debts/debts.controller.js")
-const { verifyToken } = require("./Security/JWT.js")
+const deliversController = require("../controllers/Delivers/delivers.controller.js")
 
+//Middlewares y librerias
+const { verifyToken } = require("./Security/JWT.js")
+const dayjs = require("dayjs")
+
+//Multer
 const multer = require("multer")
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
 
+//Rutas de Clientes
 clientsRouter.post("/create-client", verifyToken, (req, res, next) => {
     const {
         client_fullname,
@@ -103,6 +111,8 @@ clientsRouter.get("/get-client-financial-data", verifyToken, (req, res, next) =>
     next()
 }, clientsController.getClientFinancialData)
 
+//Rutas de clientes-deudas
+
 clientsRouter.post("/debts/create-debt", verifyToken, upload.none(), (req, res, next) => {
     const { debt_date, debt_products, client_id } = req.body;
 
@@ -170,5 +180,38 @@ clientsRouter.delete("/debts/delete-debt", verifyToken, (req, res, next) => {
     next()
 }, debtsController.deleteDebt)
 
+
+//Rutas de clientes-entregas
+clientsRouter.post("/delivers/create-deliver",verifyToken, (req, res, next) => {
+    const { 
+        deliver_date, 
+        deliver_amount, 
+        deliver_client_id,
+    } = req.body
+
+    const user_id = req.user_id
+    
+    if(!user_id){
+        return res.status(401).json({msg: "No autorizado, espere unos segundos y vuelva a intentarlo"})
+    }
+
+    if(!deliver_client_id ){
+        return res.status(400).json({msg: "El servidor no recibio el ID del cliente. Recargue la sección e intente nuevamente"})
+    }
+
+    if(!deliver_date || !deliver_amount){
+        return res.status(400).json({msg: "Todos los campos son obligatorios"})
+    }
+
+    if(!dayjs(deliver_date).isValid()){
+        return res.status(400).json({msg: "La fecha no tiene un formato valido"})
+    }
+
+    if(isNaN(parseFloat(deliver_amount)) || parseFloat(deliver_amount) <= 0){
+        return res.status(400).json({msg: "El monto de entrega debe ser un numero válido"})
+    }
+
+    next()
+}, deliversController.createDeliver)
 
 module.exports = clientsRouter
