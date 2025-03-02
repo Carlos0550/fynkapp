7e1e4dd9e6b9f55de90657afb42831570ddfb701
carlos0550 <carlospelinski03@gmail.com>
2025-03-02 10:59:04 -0300

    Feat(delivers): Trae informacion de las entregas

--- server/controllers/Clients/Queries/getClientFinancialData.sql
+++ server/controllers/Clients/Queries/getClientFinancialData.sql
@@ -1,4 +1,8 @@
 SELECT 
     (SELECT json_agg(d) 
      FROM debts d 
-     WHERE d.client_debt_id = $1 AND d.fk_user_id = $2) AS clientDebts;
+     WHERE d.client_debt_id = $1 AND d.fk_user_id = $2) AS clientDebts,
+    
+    (SELECT json_agg(d) 
+     FROM delivers d 
+     WHERE d.deliver_client_id = $1 AND d.deliver_user_id = $2) AS clientDelivers;
