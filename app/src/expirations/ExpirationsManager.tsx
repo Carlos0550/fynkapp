b0a08d53b2202835be3480e04b3dbf8f6e6e97f9
carlos0550 <carlospelinski03@gmail.com>
2025-04-24 10:31:34 -0300

    feat: administrador de vencimientos

--- app/src/expirations/ExpirationsManager.tsx
+++ app/src/expirations/ExpirationsManager.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./ExpirationsManager.css"
 
+import { Tabs } from "@mantine/core"
+import Expired from './components/Expired/Expired';
+import ToOvercome from './components/ToOvercome/ToOvercome';
 function ExpirationsManager() {
+  const [activeTab, setActiveTab] = useState<string | null>('t1');
+  const [loadingExpirations, setLoadingExpirations] = useState(false)
   return (
-    <div>
+    <div className='expirations-container'>
       <h3>Sección de vencimientos</h3>
-      <p>Pronto tendrás tu sección de vencimientos listo!</p>
+      <Tabs value={activeTab} onChange={setActiveTab}>
+        <Tabs.List>
+          <Tabs.Tab value="t1">Vencidos</Tabs.Tab>
+          <Tabs.Tab value="t2">Por vencer</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="t1">
+          <p className='expirations-title'>Deudas vencidas</p>
+          <Expired />
+        </Tabs.Panel>
+        <Tabs.Panel value="t2">
+          <p className='expirations-title'>Próximos a vencer dentro de 7 días</p>
+
+          <ToOvercome />
+        </Tabs.Panel>
+      </Tabs>
     </div>
   )
 }
