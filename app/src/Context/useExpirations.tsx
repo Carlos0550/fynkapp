e2d19bdee0d6c395ebce0b8a72ae35e71b212fc6
carlos0550 <carlospelinski03@gmail.com>
2025-04-26 23:11:03 -0300

    feat: obtiene vencimientos y expone una lista

--- app/src/Context/useExpirations.tsx
+++ app/src/Context/useExpirations.tsx
@@ -1,9 +1,15 @@
 import React, { useCallback, useMemo, useState } from 'react'
 import { logic_apis } from '../apis'
 import { showNotification } from '@mantine/notifications'
+import { ExpirationClient } from './Typescript/ExpirationTypes'
 
 function useExpirations() {
     const [token] = useState(localStorage.getItem("token") || "")
+    const [expirations, setExpirations] = useState<ExpirationClient>({
+        toOvercome: [],
+        expired: []
+    })
+
     const getExpirations = useCallback(async () => {
         const url = new URL(logic_apis.expirations + "/get-expirations")
         try {
@@ -14,11 +20,24 @@ function useExpirations() {
             })
             const responseData = await response.json()
 
+            if(response.status === 404){
+                setExpirations({
+                    toOvercome:[],
+                    expired: []
+                })
+
+                return true
+            }
             if (!response.ok) {
                 throw new Error(responseData.msg || "Error desconocido")
             }
 
-            console.log(responseData)
+            setExpirations({
+                toOvercome: responseData.toOvercome,
+                expired: responseData.expired
+            })
+
+            return true
         } catch (error) {
             console.log(error)
             showNotification({
@@ -28,13 +47,16 @@ function useExpirations() {
                 autoClose: 5000,
                 position: "top-right"
             })
+
+            return false
         }
     }, [token])
-    return useMemo(() => {
-        getExpirations
-    }, [
-        getExpirations
+    return useMemo(() => ({
+        getExpirations, expirations
+    }), [
+        getExpirations, expirations
     ])
+    
 }
 
 export default useExpirations
