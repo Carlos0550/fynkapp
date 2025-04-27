648c209075d6dd639729a028a30ff2f60079fc0c
carlos0550 <carlospelinski03@gmail.com>
2025-04-26 23:12:06 -0300

    feat: Tabla de vencimientos

--- app/src/expirations/components/Expired/Expired.tsx
+++ app/src/expirations/components/Expired/Expired.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
-
-function Expired() {
-  return (
-    <div className="expiration-table-manager">
-        <table className='expiration-table'>
-            <thead>
-                <tr>
-                    <th>Cliente</th>
-                    <th>Fecha de vencimiento</th>
-                    <th>Saldo adeudado</th>
-                    <th>Ultima entrega</th>
-                    <th>Vencido hace</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Cliente 1</td>
-                    <td>2023-03-15</td>
-                    <td>$1000.00</td>
-                    <td>2023-02-15</td>
-                    <td>7 días</td>
-                </tr>
-                <tr>
-                    <td>Cliente 2</td>
-                    <td>2023-04-15</td>
-                    <td>$2000.00</td>
-                    <td>2023-03-15</td>
-                    <td>10 días</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-  )
+import { ExpirationClient } from '../../../Context/Typescript/ExpirationTypes'
+import dayjs from "dayjs"
+interface Props {
+    expiredData: ExpirationClient["expired"]
+}
+function Expired({ expiredData }: Props) {
+    return (
+        <div className="expiration-table-manager">
+            <table className='expiration-table'>
+                <thead>
+                    <tr>
+                        <th>Cliente</th>
+                        <th>Fecha de vencimiento</th>
+                        <th>Saldo adeudado</th>
+                        <th>Ultima entrega</th>
+                        <th>Vencido hace</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {expiredData && expiredData.map((exp, idx) => (
+                        <tr key={idx}>
+                            <td>{exp.client_fullname}</td>
+                            <td>{dayjs(exp.exp_date).format("DD-MM-YYYY")}</td>
+                            <td>N/A</td>
+                            <td>{exp.last_deliver_date || "N/A"}</td>
+                            <td>{exp.days_remaining} día/s</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Expired
