73db0a470ac41a323638b5297e5586255c61be54
carlos0550 <carlospelinski03@gmail.com>
2025-04-26 23:14:06 -0300

    refactor: soluciona bug traia duplicados

--- server/src/controllers/Debts/Queries/findClientsForDebts.sql
+++ server/src/controllers/Debts/Queries/findClientsForDebts.sql
@@ -20,11 +20,13 @@ SELECT
     c.client_fullname,
     c.client_dni,
     COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) AS debt_amount,
-    COALESCE(d.debt_status, 'no_debt') AS debt_status
+    CASE
+        WHEN COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) > 0 THEN 'active'
+        ELSE 'no_debt'
+    END AS debt_status
 FROM clients c
 LEFT JOIN total_debts td ON c.client_id = td.client_debt_id
 LEFT JOIN total_delivers td2 ON c.client_id = td2.deliver_client_id
-LEFT JOIN debts d ON c.client_id = d.client_debt_id AND d.fk_user_id = $1
 WHERE c.fk_user_id = $1 
   AND c.client_fullname ILIKE $2;
 
@@ -53,11 +55,13 @@ SELECT
     c.client_fullname,
     c.client_dni,
     COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) AS debt_amount,
-    COALESCE(d.debt_status, 'no_debt') AS debt_status
+    CASE
+        WHEN COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) > 0 THEN 'active'
+        ELSE 'no_debt'
+    END AS debt_status
 FROM clients c
 LEFT JOIN total_debts td ON c.client_id = td.client_debt_id
 LEFT JOIN total_delivers td2 ON c.client_id = td2.deliver_client_id
-LEFT JOIN debts d ON c.client_id = d.client_debt_id AND d.fk_user_id = $1
 WHERE c.fk_user_id = $1 
   AND c.client_dni = $2;
 
@@ -85,10 +89,13 @@ SELECT
     c.client_fullname,
     c.client_dni,
     COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) AS debt_amount,
-    COALESCE(d.debt_status, 'no_debt') AS debt_status
+    CASE
+        WHEN COALESCE(td.total_debt_amount, 0) - COALESCE(td2.total_deliver_amount, 0) > 0 THEN 'active'
+        ELSE 'no_debt'
+    END AS debt_status
 FROM clients c
-LEFT JOIN total_debts td ON c.client_id = td.client_debt_id
-LEFT JOIN total_delivers td2 ON c.client_id = td2.deliver_client_id
-LEFT JOIN debts d ON c.client_id = d.client_debt_id AND d.fk_user_id = $1
+LEFT JOIN total_debts td ON c.client_id = td.client_debt_id AND td.fk_user_id = $1
+LEFT JOIN total_delivers td2 ON c.client_id = td2.deliver_client_id AND td2.deliver_user_id = $1
 WHERE c.fk_user_id = $1
 LIMIT 10;
+
